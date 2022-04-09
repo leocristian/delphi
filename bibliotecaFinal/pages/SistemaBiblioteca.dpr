913d3cb914c0df7ffc0e4a8847107c5784648606
leocristian <leonardosclopes@gmail.com>
2022-04-09 18:02:03 -0300

    last commit

--- bibliotecaFinal/pages/SistemaBiblioteca.dpr
+++ bibliotecaFinal/pages/SistemaBiblioteca.dpr
@@ -8,7 +8,9 @@ uses
   u_forms in 'u_forms.pas',
   u_principal in 'u_principal.pas' {FormPrincipal},
   u_usuarios in 'usuario\u_usuarios.pas' {FormUsuarios},
-  u_clientes in 'cliente\u_clientes.pas' {FormClientes};
+  u_clientes in 'cliente\u_clientes.pas' {FormClientes},
+  u_mostrar in 'usuario\u_mostrar.pas' {UsuarioForm},
+  u_perfil in 'usuario\u_perfil.pas' {PerfilUsuario};
 
 {$R *.res}
 
@@ -21,5 +23,7 @@ begin
   Application.CreateForm(TFormPrincipal, FormPrincipal);
   Application.CreateForm(TFormUsuarios, FormUsuarios);
   Application.CreateForm(TFormClientes, FormClientes);
+  Application.CreateForm(TUsuarioForm, UsuarioForm);
+  Application.CreateForm(TPerfilUsuario, PerfilUsuario);
   Application.Run;
 end.
